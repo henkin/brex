6c98e8fc7215c78281aff0c5a7afa7ce6b44c8f8
Pavel Henkin <pavel.henkin@gmail.com>
2015-12-07 16:07:36 -0800

    bootstrap/sass/gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,30 @@ const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
- 
-gulp.task('default', () => {
-	return gulp.src('**/*.js')
-		.pipe(sourcemaps.init())
-		.pipe(babel({
-			presets: ['es2015']
-		}))
-		.pipe(concat('all.js'))
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('dist'));
+const sass = require('gulp-sass');
+
+var config = {
+	bootstrapDir: 'bower_components/bootstrap-sass',
+	publicDir: 'public',
+};
+
+gulp.task('css', function() {
+	return gulp.src('src/styles/main.scss')
+			.pipe(sass({
+				includePaths: [config.bootstrapDir + '/assets/stylesheets'],
+			}).on('error', sass.logError))
+			.pipe(gulp.dest(config.publicDir + '/stylesheets'));
 });
 
+gulp.task('fonts', function() {
+	return gulp.src(config.bootstrapDir + '/assets/fonts/**/*')
+			.pipe(gulp.dest(config.publicDir + '/fonts'));
+});
+
+gulp.task('sass', function(){
+	return gulp.src('src/styles/main.scss')
+			.pipe(sass().on('error', sass.logError))
+			.pipe(gulp.dest('public/stylesheets'))
+});
+
+gulp.task('default', ['css', 'fonts']);
